e0b0f48af0cdec7707a216ff64ef96718e583637
pera1305 <pirozhkovsergey@gmail.com>
2023-06-10 17:43:43 +0300

    calculation-form done and half of advantage

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -15,3 +15,50 @@ $( document ).ready(function() {
       ]
   });
 });
+
+mobileOnlySlider(".advantages-slider", true, false, 991);
+
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+      var slider = $($slidername);
+      var settings = {
+        mobileFirst: true,
+        dots: $dots,
+        dotsClass: 'slick-dots',
+        arrows: $arrows,
+        responsive: [
+          {
+            breakpoint: $breakpoint,
+            settings: "unslick"
+          }
+        ]
+      };
+    
+      slider.slick(settings);
+    
+      $(window).on("resize", function () {
+        if ($(window).width() > $breakpoint) {
+          return;
+        }
+        if (!slider.hasClass("slick-initialized")) {
+          return slider.slick(settings);
+        }
+      });
+    } // Mobile Only Slider
+
+
+
+
+
+
+function checkboxContainerPadding() {
+  const checkButton = document.querySelectorAll('.check-button');
+  checkButton.forEach(item => {
+      item.closest('[class*="col"]').classList.add('padding-top');
+  });
+};
+
+if (document.querySelector('.calculation-form')) {
+  checkboxContainerPadding()
+} else {
+  null
+}
